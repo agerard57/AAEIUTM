fd859d7b85e8135fff83f98811fc49f2eb740455
agerard57 <56207146+agerard57@users.noreply.github.com>
2021-01-04 14:15:42 +0100

    Final State of the page

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+function affProfile() {
+    var x = document.getElementById("textCo");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function affMenu() {
+    var x = document.getElementById("menu");
+    var element = document.getElementById("contenu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
